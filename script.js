99f4f62dac9ed8233367b8674fcb6ea95482f8db
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-13 15:38:42 +0100

    modal

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`); // querySelector only selects the first one
+const btnOpenModal = document.querySelectorAll(`.show-modal`);
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener(`click`, function () {
+    //console.log(`clicked`);
+    modal.classList.remove(`hidden`);
+    overlay.classList.remove(`hidden`);
+  });
+
+  btnCloseModal.addEventListener(`click`, function () {
+    modal.classList.add(`hidden`);
+    overlay.classList.add(`hidden`);
+  });
+
+  overlay.addEventListener(`click`, function () {
+    modal.classList.add(`hidden`);
+    overlay.classList.add(`hidden`);
+  });
+}
