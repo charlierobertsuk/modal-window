7aa612e60dfc22065d261b2112e74b6d64b7e988
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-13 15:43:08 +0100

    simplified

--- script.js
+++ script.js
@@ -4,22 +4,19 @@ const modal = document.querySelector(`.modal`);
 const overlay = document.querySelector(`.overlay`);
 const btnCloseModal = document.querySelector(`.close-modal`); // querySelector only selects the first one
 const btnOpenModal = document.querySelectorAll(`.show-modal`);
-console.log(btnOpenModal);
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+//console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener(`click`, function () {
-    //console.log(`clicked`);
-    modal.classList.remove(`hidden`);
-    overlay.classList.remove(`hidden`);
-  });
-
-  btnCloseModal.addEventListener(`click`, function () {
-    modal.classList.add(`hidden`);
-    overlay.classList.add(`hidden`);
-  });
-
-  overlay.addEventListener(`click`, function () {
-    modal.classList.add(`hidden`);
-    overlay.classList.add(`hidden`);
-  });
+  btnOpenModal[i].addEventListener(`click`, openModal);
+  btnCloseModal.addEventListener(`click`, closeModal);
+  overlay.addEventListener(`click`, closeModal);
 }
